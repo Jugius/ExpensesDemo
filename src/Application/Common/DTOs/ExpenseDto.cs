06dd802822b67fb60880ca922e3fe5d449998bf8
Andrew Slusarev <jugius@gmail.com>
2024-08-06 18:20:25 +0300

    Add Application project

---
+++ src/Application/Common/DTOs/ExpenseDto.cs
@@ -0,0 +1,11 @@
+﻿using ExpensesDemo.Domain.Enums;
+
+namespace ExpensesDemo.Application.Common.DTOs;
+public class ExpenseDto
+{
+    public int Id { get; set; }
+    public DateTime PaymentTime { get; set; }
+    public ExpenseType Type { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+}
---
+++ src/Application/Common/Interfaces/IExpensesService.cs
@@ -0,0 +1,13 @@
+﻿
+using ExpensesDemo.Application.Common.DTOs;
+using ExpensesDemo.Domain.Entities;
+
+namespace ExpensesDemo.Application.Common.Interfaces;
+public interface IExpensesService
+{
+    Task<IEnumerable<Expense>> GetAll();
+    Task<Expense> GetById(int id);
+    Task<Expense> Create(ExpenseDto dto);
+    Task<Expense> Update(ExpenseDto dto);
+    Task Delete(int id);
+}
