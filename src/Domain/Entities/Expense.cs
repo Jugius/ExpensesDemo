8a5092cb1d9940f6deee3a8d3653314063458ae3
Andrew Slusarev <jugius@gmail.com>
2024-08-06 17:36:00 +0300

    Implement Domain models

---
+++ src/Domain/Entities/Expense.cs
@@ -0,0 +1,11 @@
+﻿using ExpensesDemo.Domain.Common;
+using ExpensesDemo.Domain.Enums;
+
+namespace ExpensesDemo.Domain.Entities;
+public class Expense : BaseEntity
+{
+    public DateTime PaymentTime { get; set; }
+    public ExpenseType Type { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+}
---
+++ src/Domain/Enums/ExpenseType.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpensesDemo.Domain.Enums;
+public enum ExpenseType
+{
+    HouseholdGoods = 1, // товары для дома
+    UtilityBills = 2, // оплата коммунальных услуг
+    Groceries = 3, // продукты
+    Transportation = 4, // транспорт
+}
