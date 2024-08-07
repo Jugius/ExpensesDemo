0b4f397a2684caeb86c2ffa5aeaf3106c72c0ca7
Andrew Slusarev <jugius@gmail.com>
2024-08-07 15:46:52 +0300

    Add None

--- src/Domain/Enums/ExpenseType.cs
+++ src/Domain/Enums/ExpenseType.cs
@@ -1,6 +1,7 @@
 ﻿namespace ExpensesDemo.Domain.Enums;
 public enum ExpenseType
 {
+    None = 0,
     HouseholdGoods = 1, // товары для дома
     UtilityBills = 2, // оплата коммунальных услуг
     Groceries = 3, // продукты
